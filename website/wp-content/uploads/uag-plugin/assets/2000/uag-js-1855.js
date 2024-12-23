7f6fb0c18cae6266ecbc2dd5609716bc441040c0
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2024-12-23 17:36:03 +0100

    update

---
+++ website/wp-content/uploads/uag-plugin/assets/2000/uag-js-1855.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"fea852af","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-fea852af' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"fea852af","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-fea852af' );
+});
+ });
